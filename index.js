41a51350552512901042298ea21ea0f47491f3a4
lesh <lesh@sysphere.org>
2012-02-04 22:04:45 -0500

    bugfixes

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ var cb = exports.cb = function() {
     f.apply(this,args)
 }
 
+
 // decorates a function
 var decorate = exports.decorate = function (decorator, f) {
     return function() {
@@ -19,6 +20,20 @@ var decorate = exports.decorate = function (decorator, f) {
         return decorator.apply(this,args)
     }
 }
+
+
+/*
+// accepts multiple functions, and returns one that will call all of them when called
+var joinfun = exports.joinfun = function() {
+    var functions = toArray(arguments)
+    return function() {
+        var args = toArray(arguments);
+        _.map(functions, function(f) {
+            f.call(this,args)
+        })
+    }
+}
+*/
 
 // converts a function that accepts single argument to a function that can accept an array and be called multiple times for each element
 exports.multiArg = function() {
@@ -32,11 +47,31 @@ exports.reverseArg = function() {
     var f = args.shift()
     f.apply(this,args.reverse())
 }
+/*
+// accepts callbacks that will be called in parallel with that a functions normal callback
+exports.MakeDecorator_bindcallback = function() {
+    var callbacks = toArray(arguments);
+    return function() {
+        var args = toArray(arguments);
+        var f = args.shift()
+        if (_.last(args).constructor == Function ) {
+            callbacks = joinfun(callbacks.concat(args.pop()))
+        }
+        f.apply(this,args.concat(callbacks))
+    }
+}
 
+var bindcallback = exports.bindcallback = function() {
+    var args = toArray(arguments);
+    var f = args.shift()
+    return decorate(exports.MakeDecorator_bindcallback(args),f)
+}
+*/
+
+// will put sticky arguments to a function
 
 exports.MakeDecorator_bindargs = function() {
     var bindargs = toArray(arguments);
-
     return function() {
         var args = toArray(arguments);
         var f = args.shift()
@@ -45,9 +80,10 @@ exports.MakeDecorator_bindargs = function() {
 }
 
 
-var bindargs = exports.bindargs = 
-    function(f,args) { return decorate(exports.MakeDecorator_bindargs(args),f) }
 
+
+var bindargs = exports.bindargs = 
+    function(f,args) { return decorate(exports.MakeDecorator_bindargs.apply(this,args),f) }
 
 
 // creates decorator which will delay the execution
@@ -65,7 +101,7 @@ exports.MakeDecorator_retry = function(options) {
 
     options = _.extend({
         delay: 1000,
-        retries: 4,
+        retries: 3,
         fail: undefined, // special callback for failiures
         failcall: false // call callback on each execution, not only on success?
         
@@ -80,15 +116,16 @@ exports.MakeDecorator_retry = function(options) {
             if ((!err) || (options.retries == 0 )) { callback(err,data); return }
             if (options.failcall) { callback(err,data) }
 
-            cb(fail,err,data)
+            if (options.fail) { options.fail(err,data)}
 
             setTimeout(call,options.delay)
             options.delay += options.delay
             options.retries -= 1
         }
+        
+        args.push(cb)
 
-        var call = bindargs(f,cb)
-
+        var call = bindargs(f,args)
         call()
     }
 }
